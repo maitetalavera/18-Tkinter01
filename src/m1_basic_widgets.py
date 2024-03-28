3b721a8dc040ef29576ed1ef57de32fe962e3b44
maitetalavera <mtalavera001@student.franciscan.edu>
2024-03-27 22:09:56 -0400

    m1

--- src/m1_basic_widgets.py
+++ src/m1_basic_widgets.py
@@ -5,7 +5,7 @@ import tkinter as tk
 # In this module, all of the _todo_ items will be in one comment because you
 # will be modifying the same block of code as you go.
 #
-# TODO: 1. (2 pts)
+# DONE: 1. (2 pts)
 #
 #   First, create a tkinter window called window. This is where you will be
 #   putting all of your widgets.
@@ -16,7 +16,7 @@ import tkinter as tk
 #   Once you have done this, then change the above _TODO_ to DONE.
 #
 #
-# TODO: 2. (2 pts)
+# DONE: 2. (2 pts)
 #
 #   Now, create a basic label called label with some text in it. You decide
 #   what text you want in the label.
@@ -26,7 +26,7 @@ import tkinter as tk
 #   Once you have done this, then change the above _TODO_ to DONE.
 #
 #
-# TODO: 3. (2 pts)
+# DONE: 3. (2 pts)
 #
 #   Now, create a basic button called button with some text in it. You decide
 #   what text you want in the button.
@@ -36,7 +36,7 @@ import tkinter as tk
 #   Once you have done this, then change the above _TODO_ to DONE.
 #
 #
-# TODO: 4. (2 pts)
+# DONE: 4. (2 pts)
 #
 #   Now, create a basic entry box called entry. No need for customization here.
 #
@@ -44,3 +44,23 @@ import tkinter as tk
 #   
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+
+window=tk.Tk()
+window.title("Window")
+
+label1=tk.Label(
+    window,
+    text="Hello"
+)
+label1.pack()
+
+button1 = tk.Button(
+    window, 
+    text="Click here!"
+)
+button1.pack()
+
+entry1=tk.Entry(window)
+entry1.pack()
+
+window.mainloop()
